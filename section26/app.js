39e649cc7b91c23075732f3853588c17815b5c7a
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2022-05-01 20:57:52 +0900

    Add files via upload

---
+++ section26/app.js
@@ -0,0 +1,71 @@
+const record = document.querySelector('#record')
+
+function save(player1, player2) {
+    score1 = player1.score;
+    score2 = player2.score;
+
+    const uTag = document.createElement('ul');
+    uTag.append(`home: ${score1}: away: ${score2}`);
+    record.append(uTag);
+}
+
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    if (p1.score != 0 && p2.score != 0){
+        save(p1, p2);
+    }
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+}
+
+
