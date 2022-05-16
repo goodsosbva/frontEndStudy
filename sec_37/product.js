9a4e74ff3aa821e9e7386a308876ff8957f736df
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2022-05-16 19:58:33 +0900

    Add files via upload

---
+++ sec_37/product.js
@@ -0,0 +1,109 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    })
+
+    const productSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+            maxlength: 20
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: [0, 'Price must be positive ya dodo!']
+        },
+        onSale: {
+            type: Boolean,
+            default: false
+        },
+        categories: [String],
+        qty: {
+            online: {
+                type: Number,
+                default: 0
+            },
+            inStore: {
+                type: Number,
+                default: 0
+            }
+        },
+        size: {
+            type: String,
+            enum: ['S', 'M', 'L']
+        }
+    });
+
+    
+
+    // const bike = new Product({ name: 'Tire Pump', price: 19, categories: ['Cycling']})
+    // bike.save()
+    //     .then(data => {
+    //         console.log("it worked!");
+    //         console.log(data);
+    //     })
+    //     .catch(err => {
+    //         console.log("error!!")
+    //         console.log(err)
+    //     })
+
+    // Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: 9 }, { new: true, runValidators: true })
+    //     .then(data => {
+    //         console.log("IT WORKED!")
+    //         console.log(data);
+    //     })
+    //     .catch(err => {
+    //         console.log("OH NO ERROR!")
+    //         console.log(err)
+    //     })
+
+    productSchema.methods.greet = function () {
+        console.log("HELLLO!!! HI!! HOWDY!!! ")
+        console.log(`- from ${this.name}`)
+    }
+
+    productSchema.methods.toggleOnSale = function () {
+        this.onSale = !this.onSale;
+        return this.save();
+    }
+
+    productSchema.methods.addCategory = function (newCat) {
+        this.categories.push(newCat);
+        return this.save();
+    }
+
+    productSchema.statics.fireSale = function () {
+        return this.updateMany({}, { onSale: true, price: 0 })
+    }
+
+    const Product = mongoose.model('Product', productSchema);
+
+
+    // const p = new Product({name: 'bike bag', price: 10})
+
+    // Product.insertMany([
+    //     { name: 'Tire Pump' }, { price: 9 }, { new: true, runValidators: true },
+    //     { name: 'Mountain Bike' }, { price: 19 }, { new: true, runValidators: true },
+    //     { name: 'abc' }, { price: 21 }, { new: true, runValidators: true },
+
+    // ])
+
+    const findProduct = async () => {
+        const foundProduct = await Product.findOne({ name: 'Mountain Bike' });
+        // foundProduct.greet();
+
+        console.log(foundProduct)
+        await foundProduct.toggleOnSale();
+        console.log(foundProduct)
+        // await foundProduct.addCategory('Outdoors')
+        // console.log(foundProduct)
+    }
+
+    Product.fireSale().then(res => console.log(res))
+    // findProduct();
